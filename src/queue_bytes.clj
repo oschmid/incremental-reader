90a9b3f37d9dad76123267be0220f7e48d202f75
Replit user <>
2023-11-02 05:17:45 +0000

    Test queue_bytes

--- src/queue_bytes.clj
+++ src/queue_bytes.clj
@@ -16,16 +16,12 @@
         low (.getLong bb)]
     (java.util.UUID. high low)))
 
-; TODO fix (concat-byte-arrays (uuid->bytes (java.util.UUID/randomUUID)) [])
-;; Execution error (IllegalArgumentException) at queue-bytes/concat-byte-arrays (queue_bytes.clj:26).
-;; No matching method put found taking 1 args for class java.nio.HeapByteBuffer
-
-;; https://stackoverflow.com/a/26670298
+;; https://stackoverflow.com/a/26670298 by Antti Karanta
 (defn concat-byte-arrays [& byte-arrays]
   (when (not-empty byte-arrays)
     (let [total-size (reduce + (map count byte-arrays))
-          result     (byte-array total-size)
-          bb         (java.nio.ByteBuffer/wrap result)]
+          result (byte-array total-size)
+          bb (java.nio.ByteBuffer/wrap result)]
       (doseq [ba byte-arrays]
         (.put bb ba))
       result)))
--- test/queue_bytes_test.clj
+++ test/queue_bytes_test.clj
@@ -1,16 +1,52 @@
 (ns queue-bytes-test
-  (:require [clojure.test :refer [deftest is]]
+  (:require [clojure.string :as str]
+            [clojure.test :refer [deftest is]]
+            [clojure.test.check.clojure-test :refer [defspec]]
+            [clojure.test.check.generators :as gen]
+            [clojure.test.check.properties :as prop]
             [queue-bytes :refer [bytes->uuid uuid->bytes concat-byte-arrays]]))
 
-(def uuid (java.util.UUID/randomUUID))
+;; Generate deterministic type-4 UUIDs with Clojure's test.check
+;; https://gist.github.com/jcf/7a91c9473b57b1834704
+(def gen-hex-char
+  (gen/elements (concat (map char (range (int \a) (int \f)))
+                        (range 0 9))))
 
-(deftest uuid-bytes-conversion
-  (is (= uuid (bytes->uuid (uuid->bytes uuid)))))
+(defn gen-hex-string [len]
+  (gen/fmap str/join (gen/vector gen-hex-char len len)))
+
+(def gen-uuid
+  (->> (gen/tuple (gen-hex-string 8)
+                  (gen/return "-")
+                  (gen-hex-string 4)
+                  (gen/return "-4")
+                  (gen-hex-string 3)
+                  (gen/return "-a")
+                  (gen-hex-string 3)
+                  (gen/return "-")
+                  (gen-hex-string 12))
+       (gen/fmap str/join)
+       (gen/fmap (fn [xs] (java.util.UUID/fromString (apply str xs))))))
+;; End of gist
+
+
+(deftest bytes-to-and-from-uuid
+  (is (thrown? java.lang.NullPointerException (bytes->uuid nil)))
+  (is (thrown? java.nio.BufferUnderflowException (bytes->uuid (byte-array 0))))
+  (is (thrown? java.lang.NullPointerException (uuid->bytes nil))))
+
+(defspec uuid-bytes-conversion 10
+  (prop/for-all [uuid gen-uuid]
+    (= uuid (bytes->uuid (uuid->bytes uuid)))))
 
 (deftest concat-byte-arrays-test
+  (is (= (seq (byte-array 0)) (seq (concat-byte-arrays (byte-array 0)))))
+  (is (= (seq (byte-array [(byte 0x43)])) (seq (concat-byte-arrays (byte-array [(byte 0x43)])))))
   (is (= (seq (byte-array 0)) (seq (concat-byte-arrays (byte-array 0) (byte-array 0)))))
-         ; TODO concat two values
-         ; TODO concat nils
-         ; TODO concat one nil first
-          ; TODO concat one nil second
-         )
+  (is (= (seq (byte-array [(byte 0x43) (byte 0x6c) (byte 0x6f)]))
+         (seq (concat-byte-arrays (byte-array [(byte 0x43)])
+                                  (byte-array [(byte 0x6c)])
+                                  (byte-array [(byte 0x6f)])))))
+  (is (thrown? java.lang.NullPointerException (concat-byte-arrays nil nil)))
+  (is (thrown? java.lang.NullPointerException (concat-byte-arrays nil (byte-array [(byte 0x6c)]))))
+  (is (thrown? java.lang.NullPointerException (concat-byte-arrays (byte-array [(byte 0x6c)]) nil))))
