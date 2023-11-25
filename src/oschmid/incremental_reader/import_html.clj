54bbbc86ae4970e02822a1a102a26e9f01a9f79f
Oliver Schmid <oliver.schmd@gmail.com>
2023-11-25 06:42:12 -0800

    Switch to Safelist/relaxed

--- src/oschmid/incremental_reader/import_html.clj
+++ src/oschmid/incremental_reader/import_html.clj
@@ -11,10 +11,11 @@
     (catch URISyntaxException _ nil)
     (catch MalformedURLException _ nil)))
 
+; Allows a full range of text and structural body HTML: a, b, blockquote, br, caption, cite, code, col, colgroup, dd, div, dl, dt, em, h1, h2, h3, h4, h5, h6, i, img, li, ol, p, pre, q, small, span, strike, strong, sub, sup, table, tbody, td, tfoot, th, thead, tr, u, ul
 (defn scrape "Scrape text and images" [url]
   (-> (Jsoup/connect url) ; default timeout is 30 seconds
       (.get)
       (.outerHtml)
       ; TODO add domain specific filters (e.g. wikipedia) based on Anki IR plugin
-      (Jsoup/clean (Safelist/basicWithImages)))) ; TODO get safelist from Anki IR plugin
+      (Jsoup/clean (Safelist/relaxed)))) ; TODO _ share safelist with TopicReader schema
 
